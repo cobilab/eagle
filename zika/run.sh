c6cd4c6a6d8831b2ec76dfee3d49856df0a981a0
pratas <pratas@ua.pt>
2016-02-03 17:24:11 +0000

    zikafullaut0

--- zika/run.sh
+++ zika/run.sh
@@ -27,7 +27,7 @@ cd ../
 fi
 ###############################################################################
 if [[ "$DOWNLOAD" -eq "1" ]]; then
-. GetHumanParse.sh
+#. GetHumanParse.sh
 rm -fr HS-GENOME REPORT-SPLIT;
 for((x=1; x<28; ++x));
   do 
@@ -36,15 +36,19 @@ for((x=1; x<28; ++x));
 perl DownloadZika.pl
 (./goose-splitreads < zika.fa ) &> REPORT-SPLIT;
 NZIKAS=`cat REPORT-SPLIT | tail -n 1 | awk '{print $1;}'`;
-for((x=1; x<=$NZIKAS ; ++x));
+ZNAMES="";
+for((x=1; x<$NZIKAS ; ++x));
   do
-  #cat zika.fa | grep -v ">" | tr -d -c "ACGT" > ZIKA-GENOME;
-  echo "cat out$x.fa";
+  echo "Filtering $x ...";
+  cat out$x.fa | grep -v ">" | tr -d -c "ACGT" > Z$x;
+  ZNAMES+="Z$x:"; 
   done
+cat out$NZIKAS.fa | grep -v ">" | tr -d -c "ACGT" > Z$NZIKAS;
+ZNAMES+="Z$NZIKAS";
 fi
 ###############################################################################
 if [[ "$EAGLE" -eq "1" ]]; then
-./EAGLE -v -i -t -min 11 -max 15 -r HS-GENOME ZIKA-GENOME
+./EAGLE -v -i -t -min 11 -max 15 -r HS-GENOME $ZNAMES
 fi
 # cat ZIKA-GENOME-k12.eg
 #===============================================================================
