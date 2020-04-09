5cb536fa9dcc0982bc9bab2a10b853b19508d183
jorgeMFS <jorge.miguel.ferreira.silva@gmail.com>
2020-04-09 19:49:50 +0100

    install_requirements

---
+++ scripts/install_requirements.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#retrieved from: https://www.ncbi.nlm.nih.gov/books/NBK179288/
+
+cd ~
+  /bin/bash
+  perl -MNet::FTP -e \
+    '$ftp = new Net::FTP("ftp.ncbi.nlm.nih.gov", Passive => 1);
+     $ftp->login; $ftp->binary;
+     $ftp->get("/entrez/entrezdirect/edirect.tar.gz");'
+  gunzip -c edirect.tar.gz | tar xf -
+  rm edirect.tar.gz
+  builtin exit
+  export PATH=${PATH}:$HOME/edirect >& /dev/null || setenv PATH "${PATH}:$HOME/edirect"
+  ./edirect/setup.sh
