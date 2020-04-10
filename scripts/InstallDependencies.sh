3c70bdea5a25171054e289c46b648d51af309cff
Pratas <pratas@ua.pt>
2020-04-10 18:08:23 +0100

    dependencies

---
+++ scripts/InstallDependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+conda install -c cobilab gto --yes
+conda install -c bioconda tabix --yes
+conda install -c bioconda bowtie2 --yes
+conda install -c bioconda samtools --yes
+conda install -c bioconda entrez-direct --yes
+conda install -c bioconda/label/cf201901 entrez-direct --yes
